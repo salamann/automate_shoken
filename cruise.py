1c9feec93b2fb8136b3652805d9f59d9fb246d6c
salamann <shunyo44@gmail.com>
2022-08-21 12:30:46 +0900

    fix bug of parenthesis

--- cruise.py
+++ cruise.py
@@ -41,7 +41,7 @@ def webdriver_start(mode='h') -> WebDriver:
         options.add_argument(
             'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36')
 
-        return webdriver.Chrome(Service(ChromeDriverManager().install(), options=options))
+        return webdriver.Chrome(Service(ChromeDriverManager().install()), options=options)
 
     # normal mode
     if mode == "n":
